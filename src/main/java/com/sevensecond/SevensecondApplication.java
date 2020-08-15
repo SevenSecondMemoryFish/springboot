bc54d800dd9615d500ef973d0df553482c2cf9fa
wsj <593216709@qq.com>
2020-08-15 16:57:21 +0800

    create

---
+++ src/main/java/com/sevensecond/SevensecondApplication.java
@@ -0,0 +1,13 @@
+package com.sevensecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevensecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevensecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sevensecond/SevensecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevensecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevensecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
